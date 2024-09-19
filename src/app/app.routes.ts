4941925e036b4da978122e567be9315d4260a81f
Julian <agudelo.julian@correounivalle.edu.co>
2024-09-19 15:26:09 -0500

    estructura carpetas peluquero y cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    loadChildren: () => import('./peluquero/tabs peluquero/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./peluquero/login/login.page').then( m => m.LoginPage)
+  },
+
 ];
--- src/app/peluquero/tabs peluquero/tabs.routes.ts
+++ src/app/peluquero/tabs peluquero/tabs.routes.ts
@@ -9,17 +9,22 @@ export const routes: Routes = [
       {
         path: 'tab1',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('../fila/tab1.page').then((m) => m.Tab1Page),
       },
       {
         path: 'tab2',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../servicios/tab2.page').then((m) => m.Tab2Page),
       },
       {
         path: 'tab3',
         loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('../reservas/tab3.page').then((m) => m.Tab3Page),
+      },
+      {
+        path: 'tab4',
+        loadComponent: () =>
+          import('../perfil/tab4.page').then((m) => m.Tab4Page),
       },
       {
         path: '',
