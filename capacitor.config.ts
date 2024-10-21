5ecc9a4cc4f9cb06247ef385f60ce26e9d55919b
obregon-jose <jo46646@gmail.com>
2024-10-20 20:50:35 -0500

    Refactor code to use correct package name in ExampleInstrumentedTest

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  // appId: 'io.ionic.starter',
-  appId: 'com.josh.neobarber',
+  appId: 'io.ionic.starter',
+  // appId: 'com.josh.neobarber',
   appName: 'NeoBarber',
   webDir: 'www',
+  bundledWebRuntime: false,
   // plugins: {
   //   SplashScreen: {
   //     launchShowDuration: 0,
