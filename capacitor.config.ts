efbe644f9c6c1e3805f38857d0aca5baacbdd649
obregon-jose <jo46646@gmail.com>
2024-10-26 22:18:34 -0500

    Instalación de capacitor y algunas dependencias

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'NeoBarber',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    cleartext: true
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
