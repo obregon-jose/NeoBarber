5f8e801daffe98a53883536c0d346b39bc549311
Faveste Valencia <Favian.saa@correounivalle.edu.co>
2024-10-29 13:07:40 -0500

    Validar reserva DD/MM/AAAA actual

--- src/app/peluquero/reservar/fechayhora/fechayhora.page.ts
+++ src/app/peluquero/reservar/fechayhora/fechayhora.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonSelect, IonSelectOption, IonDatetime, IonProgressBar } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
-import { NavController } from '@ionic/angular';
+import { NavController ,AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-fechayhora',
@@ -21,7 +21,8 @@ import { NavController } from '@ionic/angular';
     IonSelectOption,
     IonDatetime,
     IonProgressBar,
-    FormsModule
+    FormsModule,
+    
   ]
 })
 export class FechaYHoraPage implements OnInit {
@@ -31,11 +32,17 @@ export class FechaYHoraPage implements OnInit {
   minDate: string;
   maxDate: string;
   barberId: string = '';
+  showErrorMessage: boolean = false;
 
-  constructor(private navCtrl: NavController, private route: ActivatedRoute) {
+
+  constructor(private navCtrl: NavController, private route: ActivatedRoute ,  private alertController: AlertController) {
     const today = new Date();
     this.minDate = today.toISOString().split('T')[0];
     this.maxDate = new Date(today.getFullYear(), today.getMonth() + 1, 0).toISOString().split('T')[0];
+     // Inicializar selectedDate con la fecha actual en formato ISO para que muestre el año y la fecha actual
+     const currentDate = new Date();
+     this.selectedDate = currentDate.toISOString();
+   
   }
 
   ngOnInit() {
@@ -51,6 +58,7 @@ export class FechaYHoraPage implements OnInit {
 
   onDateChange(event: any) {
     const dateString = event.detail.value;
+    
     if (dateString) {
       this.selectedDate = dateString;
     } else {
@@ -58,13 +66,49 @@ export class FechaYHoraPage implements OnInit {
     }
   }
 
-  confirmSelection() {
+ /*  confirmSelection() {
     console.log('Fecha seleccionada:', this.selectedDate);
     console.log('Hora seleccionada:', this.selectedTime);
     console.log('Barbero seleccionado:', this.barberName);
     this.navCtrl.navigateForward('/peluquero/reservar/servicio', {
       queryParams: { barberName: this.barberName, selectedDate: this.selectedDate, selectedTime: this.selectedTime }
     });
+  }
+
+  volver() {
+    this.navCtrl.navigateBack('/peluquero/reservar/seleccionarbarbero');
+  }
+} */
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Campos requeridos',
+      message: 'Por favor, selecciona una fecha y una hora para continuar.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+
+  confirmSelection() {
+    if (!this.selectedDate || !this.selectedTime) {
+      this.showErrorMessage = true;
+      console.log(("selecione todos los campos"))
+      //console.log(alert("selecione todos los campos"))
+      this.presentAlert();
+
+
+  
+    } else {
+      this.showErrorMessage = false;
+      console.log('Fecha seleccionada:', this.selectedDate);
+      console.log('Hora seleccionada:', this.selectedTime);
+      console.log('Barbero seleccionado:', this.barberName);
+      this.navCtrl.navigateForward('/peluquero/reservar/servicio', {
+        queryParams: { barberName: this.barberName, selectedDate: this.selectedDate, selectedTime: this.selectedTime }
+      });
+    }
   }
 
   volver() {
