f0446e437de7cd07918bf7b6c84a96d64bdedcff
obregon-jose <jo46646@gmail.com>
2024-11-05 13:40:33 -0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:8000/api',
-  // apiURL: 'https://barberia1.mandanga.co/public/api'
+  // apiURL: 'http://localhost:8000/api',
+  apiURL: 'https://barberia1.mandanga.co/public/api'
 };
 
 /*
