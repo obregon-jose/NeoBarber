f20959cad122cf53b9f82ae3a609ea48bb15ba72
wbedoya30 <williamestebang@hotmail.com>
2024-11-05 13:48:09 -0500

    imagen de la app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api'
-  //apiUrl: 'http://barberia1.mandanga.co/public/api' 
+  //apiUrl: 'http://localhost:8000/api'
+  apiUrl: 'https://barberia1.mandanga.co/public/api' 
 
 };
 
