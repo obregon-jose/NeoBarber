87377a76b34f61d581058bc6dc0f8c42d93c4edd
santiagosaav <santiagosaav07@gmail.com>
2024-11-18 15:23:35 -0500

    Pagina Home

--- src/app/pages/home/client/client.page.ts
+++ src/app/pages/home/client/client.page.ts
@@ -1,25 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonAccordionGroup, IonAccordion, IonItem, IonIcon, IonLabel, IonButton, IonCard, IonCardContent, IonList, IonCardTitle, IonCardHeader, IonCardSubtitle } from '@ionic/angular/standalone';
+import { Router, RouterLink } from '@angular/router';
+
+import { 
+  IonContent, IonHeader, IonTitle, IonToolbar, 
+  IonCard, IonCardContent, IonCardHeader, IonCardTitle, 
+  IonButton, IonGrid, IonRow, IonCol, IonIcon, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.page.html',
   styleUrls: ['./client.page.scss'],
   standalone: true,
-  imports: [IonCardSubtitle, IonCardHeader, IonCardTitle, IonList, IonCardContent, IonCard, IonButton, IonLabel, IonIcon, IonItem, IonAccordion, IonAccordionGroup, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, RouterLink,
+    IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonCardHeader, IonCardTitle,
+    IonButton, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar,
+    CommonModule, FormsModule
+  ]
 })
 export class ClientPage implements OnInit {
-  client: any = {
-    peluquero: 'Juan Pérez',
-    fecha: '2023-10-01',
-    hora: '10:00 AM'
+  // Información de la próxima cita
+  nextAppointment = {
+    peluquero: 'Pepe López',
+    fecha: '2024-11-20',
+    hora: '3:00 PM'
   };
 
-  constructor() { }
+  // Lista de imágenes
+  images = [
+    'assets/images/Corte1.jpg',
+    'assets/images/Corte2.jpeg',
+    'assets/images/corte3.jpeg',
+    'assets/images/corte4.jpeg'
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  // Métodos para los botones
+  reservarCita() {
+    console.log('Navegando a la página de Reservar Cita...');
   }
 
+  editarPerfil() {
+    console.log('Navegando a la página de Editar Perfil...');
+  }
+
+  citaRapida() {
+    console.log('Iniciando Cita Rápida...');
+  }
 }
