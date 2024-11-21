d4339811c4e61c35a1020d760966f5bca86d0060
wbedoya30 <williamestebang@hotmail.com>
2024-11-21 15:54:12 -0500

    correción bug de fila,reservas, agregar boton para cancelar

--- src/app/pages/reservar/reservar.page.ts
+++ src/app/pages/reservar/reservar.page.ts
@@ -1,5 +1,9 @@
 import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonBadge, IonItem, IonCardContent, IonLabel, IonIcon, IonFabButton, IonFab, IonList } from '@ionic/angular/standalone';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCardSubtitle, 
+  IonCardTitle, IonCardHeader, IonCard, IonBadge, IonItem, IonCardContent, 
+  IonLabel, IonIcon, IonFabButton, IonFab, IonList, IonPopover 
+} from '@ionic/angular/standalone';
 import { Router, RouterLink } from '@angular/router';
 import { SeleccionarBarberoPage } from "./seleccionbarbero/seleccionarbarbero.page"; // Importa Router para la navegación
 import { ResumenPage } from './resumen/resumen.page';
@@ -7,8 +11,7 @@ import { ReservarService } from 'src/app/services/reservar/reservar.service';
 import { Preferences } from '@capacitor/preferences';
 import { CommonModule } from '@angular/common';
 import { addIcons } from 'ionicons';
-import { add, calendar, alarm, person } from 'ionicons/icons';
-
+import { add, calendar, alarm, person, ellipsisVertical, createOutline, trashOutline, ellipsisHorizontal } from 'ionicons/icons';
 
 
 @Component({
@@ -16,18 +19,21 @@ import { add, calendar, alarm, person } from 'ionicons/icons';
   templateUrl: './reservar.page.html',
   styleUrls: ['./reservar.page.scss'],
   standalone: true,
-  imports: [IonList, IonFab, IonFabButton, IonIcon, IonLabel, IonCardContent, IonItem, IonBadge, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonHeader, IonToolbar, IonTitle, IonContent,
-    RouterLink,CommonModule
+  imports: [    IonList, IonFab, IonFabButton, IonIcon, IonLabel, IonCardContent, IonItem, IonBadge, 
+    IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonHeader, IonToolbar, 
+    IonTitle, IonContent, IonPopover, RouterLink, CommonModule
   ],
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class ReservarPage  implements OnInit  {
   reservas: any[] = [];
-
+  isPopoverOpen = false;
+  selectedReserva: any;
+  popoverEvent: any;
   constructor(
     private _reservarService:ReservarService,
   ) {
-    addIcons({add,person,alarm,calendar,});
+    addIcons({ellipsisHorizontal,createOutline,trashOutline,add,person,alarm,calendar,ellipsisVertical});
   }
 
   ngOnInit() {
@@ -47,13 +53,50 @@ export class ReservarPage  implements OnInit  {
     const userAuth = value ? JSON.parse(value) : {};
     try {
       const data = await this._reservarService.cargarReservasCliente(userAuth.id);
-      this.reservas = data; 
-      this.reservas.sort((a, b) => b.id - a.id); // Ordena las reservas por id de forma descendente
-      console.log('reservas cliente pendientes, canceladas y completadas',this.reservas);
+      const reservasPendientes = data.filter((reserva: any) => reserva.status === 'pending')
+       .sort((a: any, b: any) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()); // Ordena pendientes por más recientes;
+      const reservasCompletadas = data.filter((reserva: any) => reserva.status === 'completed') 
+      .sort((a: any, b: any) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()); // Ordena completadas por más recientes;
+      
+      // Concatenar primero las pendientes y luego las completadas
+      this.reservas = [...reservasPendientes, ...reservasCompletadas];
+  
+      // this.reservas.sort((a, b) => b.id - a.id); // Ordena las reservas por id de forma descendente
+      console.log('reservas cliente pendientes y completadas',this.reservas);
        
     } catch (error) {
       console.error('Error al cargar los servicios', error);
     }
   }
+
+  openMenu(event: Event, reserva: any) {
+    this.selectedReserva = reserva;
+    this.isPopoverOpen = true;
+  }
+
+  closeMenu() {
+    this.isPopoverOpen = false;
+  }
+
+  editarReserva(reserva: any) {
+    console.log('Editar reserva:', reserva);
+    // Lógica para editar la reserva
+    this.closeMenu();
+  }
+
+  cancelarReserva(reserva: any) {
+    console.log('Cancelar reserva:', reserva);
+    // Lógica para cancelar la reserva
+    this.closeMenu();
+  }
+  async presentPopover(event: Event) {
+    this.popoverEvent = event;
+    this.isPopoverOpen = true;
+  }
+
+  closePopover() {
+    this.isPopoverOpen = false;
+  }
+  
 }
     
